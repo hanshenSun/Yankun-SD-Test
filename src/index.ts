15ca568f8daf785f5fe3b5ca0f121e330dcb81fa
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2021-10-05 14:40:55 -0400

    update
    update from Yankun

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,48 @@
 import "reflect-metadata"
 import { api } from "@shapediver/viewer"
 
-const modelViewUrl = 'https://sddev2.eu-central-1.shapediver.com'; // PLEASE ADD YOUR MODEL VIEW URL HERE
-const ticket = 'f458732383d032fe0a479dea5e134da634c557e8d50f69621ce3f7fbd34f84c65a8b607585489f5877443f8292841a6e952c08990690cf127d169d202b098f66ee5368af94d02270f3d6d769de8e416608f80d0994b3d898a41be5f4f38a0c428699d1d7f9d9c4-6e86fe6d52d13f8f55b7b873bd75a0e6'; // PLEASE ADD YOUR TICKET HERE
+const modelViewUrl = 'https://sdr7euc1.eu-central-1.shapediver.com'; // PLEASE ADD YOUR MODEL VIEW URL HERE
+const ticket = '31c5a58b82869d47e63e72cc6db96df36d1f190c352beb3b2fe2ae3585d3d86c6b02fd82bb04dd3e06215e6e03c69b37190f9de9bf32a8ffaf872bd6486dc667a8b280e7e93336e8e5cc2ba435640f6931c2976dc294cad786aa816af94d2f647306b8cae3d7a9-48cb8393cc258a37c0b4ccfafb3e52d9'; // PLEASE ADD YOUR TICKET HERE
+
 
 (async () => {
+
+  const container = document.getElementById("container")
+  container.setAttribute("style",`width:${window.innerWidth}px`)
+  container.setAttribute("style",`height:${window.innerHeight/2}px`)
+
+
+  // create a title
+  const h1 = document.createElement("H1");
+  const text = "TO TEST SOME KARAMBA HERE!!!";
+  h1.innerHTML = text;
+  const firstChild = document.body.firstChild;
+  await document.body.insertBefore(h1, firstChild);
+  
   // create a viewer
   const viewer = await api.createViewer({ canvas: <HTMLCanvasElement>document.getElementById('canvas'), id: 'myViewer' });
   // create a session
   const session = await api.createSession({ ticket, modelViewUrl, id: 'mySession'});
 
-  // read out the parameter with the specific name
-  const lengthParameter = session.getParameterByName('Length')[0];
-  
-  // update the value
-  lengthParameter.value = 6;
-  // and customize the scene
-  await session.customize();
-  console.log(lengthParameter.value);
+  // // read out the parameter with the specific name
+  const loadParameter = session.getParameterByName('Load')[0];
 
+  // // update the value
+  var input = document.querySelector("input")
+  var rangeValue = document.getElementById("rangeValue")
+  var newLoad = input.value
+
+  input.addEventListener("input", async()=>{
+    rangeValue.innerHTML = input.value
+    newLoad = input.value
+    console.log(input.value);
+    loadParameter.value = newLoad
+  // and customize the scene
+    await session.customize();
+  })
   
-  lengthParameter.value = 3;
-  await session.customize();
-  console.log(lengthParameter.value);
+
+  // console.log(colorParameter.value);
 })();
+
+
